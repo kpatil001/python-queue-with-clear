6540ab59da9c18d65a3478d86bc7a92337d674aa
krunal.patil <krunal.patil@quantiphi.com>
2021-06-24 16:13:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Python queue with clearing functionality
+
+Queue available in python does not have functionality to clear it. Here I have implemented a class Queue which inherits from queue.Queue and has clear method in it. Hence this implementation support all the features of queue.Queue along with functionality of clearing itself.
---
+++ queue-with-clear.py
@@ -0,0 +1,16 @@
+import queue
+
+class Queue(queue.Queue):
+    #def __init__(self):
+        #Queue._init__(self)
+
+    def clear(self):
+        with self.mutex:
+            unfinished = self.unfinished_tasks - len(self.queue)
+            if unfinished <= 0:
+                if unfinished < 0:
+                    raise ValueError('task_done() called too many times')
+                self.all_tasks_done.notify_all()
+            self.unfinished_tasks = unfinished
+            self.queue.clear()
+            self.not_full.notify_all()
